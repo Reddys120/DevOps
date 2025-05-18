497e95a343e7424764a460b6a68551a9d94ba26c
Rajashekar A <rajeshekaraleti@gmail.com>
2025-05-18 11:52:53 +0530

    $ commit

--- variable.sh
+++ variable.sh
@@ -4,10 +4,12 @@ Person1=Advik
 Person2=sandhya
 1=Love
 2=Love
+3=Love
+4=Love
 
 echo "$Person1:: How are you Person2?"
 echo "$Person2:: Am good how are you doing Person1"
-echo "$Person1:: 1"
-echo "$Person2:: 2"
-echo "$Person1:: 3"
-echo "$Person2:: 4"
+echo "$Person1:: $1"
+echo "$Person2:: $2"
+echo "$Person1:: $3"
+echo "$Person2:: $4"
