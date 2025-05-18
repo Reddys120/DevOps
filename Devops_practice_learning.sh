8c25ca43bacc6ad2cc8dad4a5b03dc85572295ec
Rajashekar A <rajeshekaraleti@gmail.com>
2025-05-18 18:41:00 +0530

    $ commit

---
+++ Devops_practice_learning.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+
+
+Timestamp=$('date +%Y-%m-%d-%H-%M-%S')
+USERID=$(id -u)
+RED="\e[31m"
+GREEN="\e[32m"
+YELLOW="\e[33m"
+BLUE="\e[34m"
+LOG_FOLDER="/var/log/shell_script_logs/script.log_$date.log"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $RED FAILED"
+        exit 1
+    else
+        echo -e "$2 ... $GREEN SUCCESS" 
+    fi
+}
+
+echo "Script Execution Started at $timestamp" &>>$LOG_FOLDER
+
+if [ $USERID -ne 0 ] 
+then
+    echo "ERROR:: You must have sudo previliges"
+    exit 1 
+fi
+echo "Checking for MYSQL installation"
+dnf list installed mysql &>> $LOG_FOLDER
+
+if [ $? -ne 0 ]
+then # not installed
+    dnf install mysql -y &>> $LOG_FOLDER
+    VALIDATE $? INSTALLING MYSQL
+else
+    echo -e "MYSQL IS ALREADY $YELLOW INSTALLED"
+fi
+
+echo "Checking for GIT installation"
+dnf list installed git &>> $LOG_FOLDER
+if [ $? -ne 0 ]
+then
+    dnf install git -y &>> $LOG_FOLDER
+    VALIDATE $? INSTALLING GIT
+else
+    echo -e "GIT IS ALREADY $YELLOW INSTALLED"
+fi 
