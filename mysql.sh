ade5a31fe40a75454c3dfc08e4b1f16be46cedce
Rajashekar A <rajeshekaraleti@gmail.com>
2025-05-18 15:20:40 +0530

    $ commit

--- mysql.sh
+++ mysql.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-echo "installing Nginx in server"
+
 USERID=$(id -u)
 if [ $USERID -ne 0 ] 
 then
     echo "ERROR:: You must have sudo previliges"
-    exit 1
+    exit 1 #other than 0
 fi
 
 dnf install mysql -y
+
+if [$? -ne 0]
+then 
+echo "installing mysql failed"
+else 
+echo "installing mysql success"
+fi
+
