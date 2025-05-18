949f64e47602257a94ffecf3e3bd1d083a11408b
Rajashekar A <rajeshekaraleti@gmail.com>
2025-05-18 14:40:34 +0530

    $ commit

--- Devops_practice.sh
+++ Devops_practice.sh
@@ -2,9 +2,10 @@
 
 echo "Check for number of passed students"
 Results=("50" "60" "30" "46" "35" "80" "75")
-if [$Results -ge 35 ]; then
-echo "$Results of the stdents passed"
+Students=$1
+if [$Results -gt 35 ]; then
+echo "$Results of the $1 passed"
 else 
-echo "$Results of the students not passed"
+echo "$Results of the $1 not passed"
 fi
 echo ""
