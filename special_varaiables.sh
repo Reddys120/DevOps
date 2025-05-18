cfa0ac9cb32236629888b20fd6a195bbd1fc2042
Rajashekar A <rajeshekaraleti@gmail.com>
2025-05-18 13:31:45 +0530

    $ commit

--- special_varaiables.sh
+++ special_varaiables.sh
@@ -35,17 +35,3 @@ echo "PID of last background command: $!"
 
 # $_ - Last argument of the previous command
 echo "Last argument of last command: $_"
-
-echo ""
-
-
-Variable	Description
-echo "$0	Script name"
-echo "$1-$9	Positional arguments"
-echo "$@	All arguments (individually quoted)"
-echo "$*	All arguments (as a single string)"
-echo "$#	Number of arguments"
-echo "$?	Exit status of last command"
-echo "$$	PID of current script"
-echo "$!	PID of last background process"
-echo "$_	Last argument of last command"
