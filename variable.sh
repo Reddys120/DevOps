edbc68b106f80dea2c31a1ad29ed70e92fd7fcc8
Rajashekar A <rajeshekaraleti@gmail.com>
2025-05-18 12:21:19 +0530

    $ commit

--- variable.sh
+++ variable.sh
@@ -3,7 +3,7 @@
 Person1=$1
 Person2=$2
 Expression=Love-you
-
+Date=currentTime
 
 echo "$Person1:: How are you $Person2?"
 echo "$Person2:: Am good how are you doing $Person1"
@@ -11,3 +11,4 @@ echo "$Person1:: $Expression"
 echo "$Person2:: $Expression"
 echo "$Person1:: $Expression"
 echo "$Person2:: $Expression"
+echo "$Date"
