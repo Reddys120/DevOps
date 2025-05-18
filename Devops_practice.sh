8c8bd982d1c2fe8e9a7f69658882bb0222b24e04
Rajashekar A <rajeshekaraleti@gmail.com>
2025-05-18 16:21:33 +0530

    $ commit

--- Devops_practice.sh
+++ Devops_practice.sh
@@ -1,8 +1,51 @@
 #!/bin/bash
 
-echo "installing Nginx in server"
 USERID=(id -u)
 if [$USERID -ne 0] 
 then
     echo "ERROR:: You must have sudo previliges"
+    exit 1 
 fi
+echo "Checking for MYSQL installation"
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then # not installed
+    dnf install mysql -y
+    if [ $? -ne 0 ]
+    then
+        echo "Error:: Installation of mysql ... failed"
+        exit 1
+    else
+        echo "Installation of mysql is success" 
+    fi
+else
+    echo "Mysql is already installed"
+
+
+fi
+
+echo "Checking for GIT installation"
+dnf list installed git
+if [ $? -ne 0 ]
+then
+    dnf install git -y
+    if [ $? -ne 0 ]
+    then 
+        echo "Installation of Git Failed"
+        exit 1
+    else 
+        echo "Installation of Git is Success"
+    fi
+else
+    echo "Git is already Installed"
+fi
+
+# dnf install git -y
+# if [ $? -ne 0 ]
+# then 
+#     echo "installation of git ... failed"
+#     exit 1
+# else 
+#     echo "installation of git is success"
+# fi
