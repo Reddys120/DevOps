ce1010d35d60449f2fe05d9abc3062d4149d6a08
Rajashekar A <rajeshekaraleti@gmail.com>
2025-05-19 11:46:28 +0530

    $ commit

--- mysql.sh
+++ mysql.sh
@@ -1,4 +1,32 @@
 #!/bin/bash
 
-timestamp=$(date '+%Y-%m-%d %H:%M:%S')
-echo "Current timestamp: $timestamp"
+
+USERID=$(id -u)
+if [ $USERID -ne 0 ] 
+then
+    echo "ERROR:: You must have sudo previliges"
+    exit 1 #other than 0
+fi
+
+dnf list installed mysql
+if [ $? -ne 0 ]
+
+dnf install mysql -y
+
+if [ $? -ne 0 ]
+then 
+    echo "installing mysql....failed"
+    exit 1 #other than 0
+else 
+    echo "installing mysql....success"
+fi
+
+dnf install git -y
+
+if [ $? -ne 0 ]
+then 
+    echo "installing git failed"
+    exit 1 #other than 0
+else 
+    echo "installing git success"
+fi
