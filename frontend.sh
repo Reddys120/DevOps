790c63610439dd38f4c7893c3b0c85c80ebb5f6a
Rajashekar A <rajeshekaraleti@gmail.com>
2025-05-19 14:40:54 +0530

    $ commit

---
+++ frontend.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+Timestamp=$(date +%Y-%m-%d-%H:%M:%S)
+USERID=$(id -u)
+RED="\e[31m"
+GREEN="\e[32m"
+YELLOW="\e[33m"
+BLUE="\e[34m"
+NC='\e[0m' # No Color
+LOG_FOLDER="/var/log/shell_script_logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1)
+LOGS="$LOG_FOLDER/$LOG_FILE-$Timestamp.log"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $RED FAILED $NC"
+        exit 1
+    else
+        echo -e "$2 ... $GREEN SUCCESS $NC" 
+    fi
+}
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ] 
+    then
+        echo "ERROR:: You must have sudo previliges"
+        exit 1
+    fi 
+}
+
+echo "Script Execution Started at: $Timestamp" &>>$LOGS
+
+
+CHECK_ROOT
+
+dnf install nginx -y 
+VALIDATE $? "Nginx Installation"
+
+systemctl enable nginx
+VALIDATE $? "Nginx Service Enable"
+
+systemctl start nginx
+VALIDATE $? "Nginx Service Start"
+
+rm -rf /usr/share/nginx/html/*
+VALIDATE $? "Nginx HTML Folder Clean"
+
+curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip
+VALIDATE $? "Frontend Zip Download"
+
+cd /usr/share/nginx/html
+
+unzip /tmp/frontend.zip
+VALIDATE $? "Unzip frontend"
+
+cp /etc/nginx/default.d/expense.conf
+
+
+
+
+
