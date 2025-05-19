a7ddb851815129b4b3c695c6ecc3ec088d5c855c
Rajashekar A <rajeshekaraleti@gmail.com>
2025-05-19 18:58:53 +0530

    $ commit

--- Logs_archive.sh
+++ Logs_archive.sh
@@ -54,5 +54,9 @@ fi
 echo "Script Execution Started at: $Timestamp"
 
 LOGS_ARCHIVE=$(find $SOURCE_DIR -name "*.log" -mtime +$DAYS)
-
-echo "Logs are $LOGS_ARCHIVE"
+if [ -n $LOGS_ARCHIVE]
+then 
+     echo "Logs are $LOGS_ARCHIVE"
+else
+     echo "No logs found"
+fi
