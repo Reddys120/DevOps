49a099c577b1d045c65392e5fcff1ae15bb6eb81
Rajashekar A <rajeshekaraleti@gmail.com>
2025-05-20 13:32:14 +0530

    $ commit

--- Disk_utilization.sh
+++ Disk_utilization.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
+VALIDATE(){
+    if [ $2 -gt 5 ]
+    then
+        echo -e "$2 ... $RED ALERT $NC"
+        exit 1
+    else
+        echo -e "$2 ... $GREEN OKAY $NC" 
+    fi
+}
+
 DISK_USAGE=$(df -hT | grep xfs)
 DISK_THRESHOLD=5 # 5% threshold
 while read -r percentage
 do  
-    UTILIZATION=$(echo "$percentage" | awk -F " " '{print $6F}') 
-    echo "Percentage $UTILIZATION " 
+    UTILIZATION=$(echo "$percentage" | awk -F " " '{print $6F}')
+    PARTITION==$(echo "$percentage" | awk -F " " '{print $NF}') 
+    echo " $PARTITION $UTILIZATION  " 
 done <<< $DISK_USAGE
