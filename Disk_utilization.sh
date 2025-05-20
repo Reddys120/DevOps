a9291bd72bae44788a6114a1bd6a49113acd3a78
Rajashekar A <rajeshekaraleti@gmail.com>
2025-05-20 14:03:41 +0530

    $ commit

--- Disk_utilization.sh
+++ Disk_utilization.sh
@@ -16,4 +16,4 @@ done <<< $DISK_USAGE
 
 echo -e "Message: $MSG"
 
-echo "$MSG" | mail -s "High Disk Usage" rajeshekaraleti@gmail.com
+echo "$MSG" | mutt -s "High Disk Usage" rajeshekaraleti@gmail.com
