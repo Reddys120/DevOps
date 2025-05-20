16991016ca8318a249d46a7d67f93060bb75ce00
Rajashekar A <rajeshekaraleti@gmail.com>
2025-05-20 13:48:35 +0530

    $ commit

--- Disk_utilization.sh
+++ Disk_utilization.sh
@@ -6,5 +6,5 @@ while read -r percentage
 do  
     UTILIZATION=$(echo "$percentage" | awk -F " " '{print $6F}' | sed 's/%//g')
     PARTITION=$(echo "$percentage" | awk -F " " '{print $NF}') 
-    echo "FILESYSTEM: $PARTITION = $UTILIZATION  " 
+    echo "FILESYSTEM:$PARTITION = $UTILIZATION  " 
 done <<< $DISK_USAGE
