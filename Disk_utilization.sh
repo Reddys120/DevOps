18559ebac9b5e97260ad9764288b9993acf3c636
Rajashekar A <rajeshekaraleti@gmail.com>
2025-05-20 13:47:45 +0530

    $ commit

--- Disk_utilization.sh
+++ Disk_utilization.sh
@@ -4,7 +4,7 @@ DISK_USAGE=$(df -hT | grep xfs)
 DISK_THRESHOLD=5 # 5% threshold
 while read -r percentage
 do  
-    UTILIZATION=$(echo "$percentage" | awk -F " " '{print $6F}')
+    UTILIZATION=$(echo "$percentage" | awk -F " " '{print $6F}' | sed 's/%//g')
     PARTITION=$(echo "$percentage" | awk -F " " '{print $NF}') 
     echo "FILESYSTEM: $PARTITION = $UTILIZATION  " 
 done <<< $DISK_USAGE
