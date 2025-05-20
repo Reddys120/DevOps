b6e65b56c8e76363f3b8ced31f806553855a3f1f
Rajashekar A <rajeshekaraleti@gmail.com>
2025-05-20 13:54:34 +0530

    $ commit

--- Disk_utilization.sh
+++ Disk_utilization.sh
@@ -2,6 +2,7 @@
 
 DISK_USAGE=$(df -hT | grep xfs)
 DISK_THRESHOLD=5 # 5% threshold
+MSG=''
 while read -r percentage
 do  
     UTILIZATION=$(echo "$percentage" | awk -F " " '{print $6F}' | sed 's/%//g')
@@ -9,6 +10,6 @@ do
     echo "FILESYSTEM:$PARTITION = $UTILIZATION  "
     if [ $DISK_USAGE -ge $DISK_THRESHOLD ]
     then
-        echo " Disk utilization has reached threshold...Please check " 
+       MSG+="Disk utilization has reached threshold...Please check" 
     fi
 done <<< $DISK_USAGE
